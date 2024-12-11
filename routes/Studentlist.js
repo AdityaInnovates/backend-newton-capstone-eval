775c92630b7c121e3f84b4ef3145f41ce42574c8
Raghav Gupta 2 <raghavgupta1@Raghavs-MacBook-Pro-2.local>
2024-12-12 01:18:08 +0530

    update

---
+++ routes/Studentlist.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const {fetchStudentlist} = require('../controllers/Studentlist')
+
+const {addStudentlist} = require('../controllers/Studentlist')
+
+// const {updateStudentlist} = require('../controllers/Studentlist')
+
+const router = express.Router()
+
+router.get('/', fetchStudentlist)
+router.post('/', addStudentlist)
